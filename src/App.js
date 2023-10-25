a5b275ae6c2c0d6d016b3a01c1d2f7a0908bf8c0
Josef Polasek <josefpolasek12@gmail.com>
2023-10-25 22:57:31 +0300

    simple working version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./index.css";
+
+import profile_picture from "./media/profile_picture.jpeg";
+
+console.log(profile_picture);
 
 function App() {
+  const [email, setEmail] = useState("josefpolasek@gmail.com");
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(email);
+    alert("Email copied to clipboard!");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="container">
+      <img src={profile_picture} alt="Profile" className="image" />
+      <h1 className="headline">Josef Polasek</h1>
+      <div className="links">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/josefpolasek"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
-      </header>
+        <a
+          href="https://www.linkedin.com/in/josef-polasek-b3a403227/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </div>
+      <div>
+        {email}
+        {/* <button onClick={copyToClipboard}>Copy to Clipboard</button> */}
+      </div>
+      {/* <div className="projects">
+        <h2>Projects</h2>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/josefpolasek/hotelBooking-cpp"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Hotel room booking program
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/josefpolasek/book-collection"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Book collection in Java
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/josefpolasek/integrify-restCountries"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Rest countries API
+            </a>
+          </li>
+        </ul>
+      </div> */}
     </div>
   );
 }
